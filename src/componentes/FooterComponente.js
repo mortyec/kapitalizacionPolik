ad65b6a983543c22ce0511a894f5872fefcf4856
anthony <tony.anthony@hotmail.es>
2023-04-17 09:00:54 -0500

    cambios

--- src/componentes/FooterComponente.js
+++ src/componentes/FooterComponente.js
@@ -5,43 +5,44 @@ function FooterComponente() {
     <>
      <footer class="site-footer">
       <div class="containerfooter">
-        <div class="row">
-          <div class="col-sm-12 col-md-6">
-            <h6>Acerca De</h6>
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-          </div>
+        <img src="./img/Navbar/logoPrueba.png"/>
+          {/* <div class="row">
+            <div class="col-sm-12 col-md-6">
+              <h6>Acerca De</h6>
+              <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+            </div>
 
-          <div class="col-xs-6 col-md-3">
-            <h6>Categorías</h6>
-            <ul class="footer-links">
-              <li><a href="./#">Item 1</a></li>
-              <li><a href="./#">Item 2</a></li>
-              <li><a href="./#">Item 3</a></li>
-              <li><a href="./#">Item 4</a></li>
-            </ul>
-          </div>
+            <div class="col-xs-6 col-md-3">
+              <h6>Categorías</h6>
+              <ul class="footer-links">
+                <li><a href="./#">Item 1</a></li>
+                <li><a href="./#">Item 2</a></li>
+                <li><a href="./#">Item 3</a></li>
+                <li><a href="./#">Item 4</a></li>
+              </ul>
+            </div>
 
-          <div class="col-xs-6 col-md-3">
-            <h6>Links</h6>
-            <ul class="footer-links">
-              <li><a href="./#">Item 1</a></li>
-              <li><a href="./#">Item 2</a></li>
-              <li><a href="./#">Item 3</a></li>
-              <li><a href="./#">Item 4</a></li>
-              <li><a href="./#">Item 5</a></li>
-            </ul>
+            <div class="col-xs-6 col-md-3">
+              <h6>Links</h6>
+              <ul class="footer-links">
+                <li><a href="./#">Item 1</a></li>
+                <li><a href="./#">Item 2</a></li>
+                <li><a href="./#">Item 3</a></li>
+                <li><a href="./#">Item 4</a></li>
+                <li><a href="./#">Item 5</a></li>
+              </ul>
+            </div>
           </div>
+          <hr />
         </div>
-        <hr/>
-      </div>
-      <div class="containerfooter">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by
-         <a href="https://agenciaprez.com/"> Agencia Prez</a>.
-            </p>
-          </div>
-        </div>
+        <div class="containerfooter">
+          <div class="row">
+            <div class="col-md-8 col-sm-6 col-xs-12">
+              <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by
+                <a href="https://agenciaprez.com/"> Agencia Prez</a>.
+              </p>
+            </div>
+          </div> */}
       </div>
 </footer>
     </>
